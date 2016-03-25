adb29f8e5ffae85daa8a52d6bdc0a979c897df6d
Emmanuel Vadot <manu@bidouilliste.com>
2016-03-25 21:38:53 +0100

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fbsd_gpio',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/evadot/fbsd_gpio_py',
     license='BSD',
     author='Emmanuel Vadot',
